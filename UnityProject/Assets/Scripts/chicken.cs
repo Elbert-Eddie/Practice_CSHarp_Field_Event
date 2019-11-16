45f826781bac72825b47e9d9e4beb28fdec26d54
Elbert-Eddie <Elbert-Eddie@users.noreply.github.com>
2019-11-16 11:00:45 +0800

    作業2

---
+++ UnityProject/Assets/Scripts/chicken.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class chicken : MonoBehaviour
+{
+    private void Start()
+    {
+        print("遊戲開始!!!");
+    }
+
+    private void Update()
+    {
+        print("咕咕咕~~~");
+    }
+
+    [Header("跳躍高度"), Range(10, 2000)]
+    public int jump = 200;
+
+    [Header("移動速度"), Range(1, 100)]
+    public int move = 10;
+
+    [Header("是否取得雞蛋")]
+    public bool egg;
+
+    [Header("說話內容")]
+    public string talk = "咕咕咕~~~";
+}
